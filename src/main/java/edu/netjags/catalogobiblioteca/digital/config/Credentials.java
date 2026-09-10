9bcd2cdba1b0b18f9cfc03abe6b0f6cacde912f9
unknown <jguerra-2026492@kinal.edu.gt>
2026-09-09 21:14:41 -0600

    Clases de conexion creadas

---
+++ src/main/java/edu/netjags/catalogobiblioteca/digital/config/Credentials.java
@@ -0,0 +1,13 @@
+package com.jguerrakinal.abarroteriakinal.config;
+
+public class Credentials {
+    
+        public static final String URL_DB = "jdbc:mysql://localhost:3306/abarroteria_kinal_in4bv";
+        public static final String USER_DB = "IN4BV";
+        public static final String PASS_DB = "%IndiVA4";
+        
+        private Credentials(){
+            
+        }
+    
+}
